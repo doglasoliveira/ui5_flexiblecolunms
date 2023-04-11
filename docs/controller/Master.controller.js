b55cd6be59953f9d304a999800b819fe4f99f03d
Doglas Oliveira <doglasoliveira@Doglass-Air.lan>
2023-04-12 00:15:34 +0100

    add completed load

--- docs/controller/Master.controller.js
+++ docs/controller/Master.controller.js
@@ -21,6 +21,17 @@ sap.ui.define([
                 this._receipt = oEvent.getSource().getBindingContext("receipts").getObject().receiptId || this._receipt || "0";
                 this.getView().getModel().setProperty("/layout", "TwoColumnsMidExpanded")
                 this.oRouter.navTo("detail", { receiptId: this._receipt, layout: "TwoColumnsMidExpanded" });
+            },
+
+            andleUploadComplete: function(oEvent) {
+                var sResponse = oEvent.getParameter("response"),
+                    iHttpStatusCode = parseInt(/\d{3}/.exec(sResponse)[0]),
+                    sMessage;
+    
+                if (sResponse) {
+                    sMessage = iHttpStatusCode === 200 ? sResponse + " (Upload Success)" : sResponse + " (Upload Error)";
+                    MessageToast.show(sMessage);
+                }
             }
         });
     });
